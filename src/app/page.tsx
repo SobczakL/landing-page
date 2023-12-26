eed7ee341a3f7f74f9d6014a34b46e189d4afc2b
lucqaz01 <Lucassobczak3@gmail.com>
2023-12-26 15:53:23 -0500

    navigation + headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import PrimaryHeader from '@/components/PrimaryHeader'
+import SecondaryHeader from '@/components/SecondaryHeader'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="px-4 bg-g1">
+            <PrimaryHeader/>
+            <SecondaryHeader/>
+    </div>
   )
 }
---
+++ src/components/SecondaryHeader.tsx
@@ -0,0 +1,7 @@
+export default function SecondaryHeader(){
+    return(
+        <div>
+            <span className="secondaryHeader text-black text-transparent drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] align-end text-headerSm md:text-headerMd font-extrabold">DEVELOPER</span>
+        </div>
+    )
+}
