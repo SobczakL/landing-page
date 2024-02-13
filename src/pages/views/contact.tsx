05d22e2bdf1fed31ccf57022e911497de4874b0c
SobczakL <lucassobczak3@gmail.com>
2024-02-12 21:04:23 -0500

    contact form

--- src/pages/views/contact.tsx
+++ src/pages/views/contact.tsx
@@ -1,7 +1,19 @@
 export default function Contact() {
     return (
         <section className="bg-g5 h-full">
+            <div>
+                <p>CONTACT</p>
+                <form>
+                    <label for="name">Name*</label>
+                    <input id="name" type="text"></input><br />
+                    <label for="name">Name*</label>
+                    <input id="name" type="text"></input><br />
+                    <label for="name">Name*</label><br />
+                    <textarea id="name" type="text"></textarea>
+                </form>
 
-        </section>
+            </div>
+
+        </section >
     )
 }
