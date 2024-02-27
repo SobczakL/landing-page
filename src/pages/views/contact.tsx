d0c95f4c7cec78709e69e851a6874a5895c05a40
SobczakL <lucassobczak3@gmail.com>
2024-02-26 21:42:48 -0500

    contact form styling

--- src/pages/views/contact.tsx
+++ src/pages/views/contact.tsx
@@ -2,14 +2,21 @@ export default function Contact() {
     return (
         <section className="bg-g5 h-full">
             <div>
-                <p>CONTACT</p>
-                <form>
-                    <label for="name">Name*</label>
-                    <input id="name" type="text"></input><br />
-                    <label for="name">Name*</label>
-                    <input id="name" type="text"></input><br />
-                    <label for="name">Name*</label><br />
-                    <textarea id="name" type="text"></textarea>
+                <p className="text-black text-sectionLabelSm md:text-sectionLabelMd lg:text-sectionLabelLg">CONTACT</p>
+                <form className="flex flex-col text-black text-bodySm md:text-bodyMd lg:text-bodyLg">
+                    <div className="flex gap-4 border border-b-black">
+                        <label for="name" className="">Name *</label>
+                        <input id="name" type="text" className="bg-transparent cursor-text w-full py-0"></input><br />
+                    </div>
+                    <div className="flex gap-4 border border-b-black">
+                        <label for="email">Email *</label>
+                        <input id="email" type="email" className="bg-transparent cursor-text w-full py-0"></input><br />
+                    </div>
+                    <div className="flex gap-4 border border-b-black">
+                        <label for="msg">Message *</label><br />
+                        <textarea id="msg" type="text" className="bg-transparent cursor-text w-full py-0"></textarea><br />
+                    </div>
+                    <button className="bg-g4 rounded-lg py-1 px-4 max-w-fit">Send Message</button>
                 </form>
 
             </div>
