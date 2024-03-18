093cd079527765189d2872f3bab8554a9cca8da9
SobczakL <lucassobczak3@gmail.com>
2024-03-17 23:02:17 -0400

    reworked home in mobile view with added copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,22 @@ import useCurrentTime from '@/utils/useCurrentTime';
 export default function Home() {
     const currentTime = useCurrentTime();
     return (
-        <div className="box-border grid grid-rows-[1fr, 2fr, 1fr] grid-cols-3 px-4 md:px-10 lg:px-16 pt-8 pb-24 bg-g1 font-sans text-black h-full">
-            <div className='col-span-3 md:col-span-1'>
-                <PrimaryHeader />
-            </div>
-            <div className='col-span-3 md:col-start-2 md:col-span-2 self-center justify-self-center'>
-                <img className="" src='/images/headshot.png' alt='headshot' />
-            </div>
+        <div className="self-center box-border gap-8 grid grid-rows-[1fr, 2fr, 1fr] grid-cols-3 px-4 md:px-10 lg:px-16 pt-10 pb-24 bg-g1 font-sans text-black h-full">
             <div className='col-span-2'>
                 <SecondaryHeader />
             </div>
-            <div className="text-black justify-self-end">
-                <p className='w-fit'>{currentTime}</p>
-                <p className='w-fit'>TORONTO, ON,<br /> CANADA</p>
+            <div className='text-bodySm col-span-2'>
+                <p>As a dedicated full stack developer, I specialize in crafting innovative tech solutions tailored to our interconnected world.</p>
+            </div>
+            <div className="flex flex-col justify-between text-black col-span-1 justify-self-end text-right">
+                <p className='w-fit text-bodySm'>TORONTO,<br /> CANADA</p>
+                <p className='w-fit ml-auto text-bodySm'>{currentTime}</p>
+            </div>
+            <div className='col-span-3 md:col-start-2 md:col-span-2 self-center justify-self-center md:justify-self-end md:self-end'>
+                <img className="md:max-h-96 lg:max-h-[]" src='/images/headshot.png' alt='headshot' />
+            </div>
+            <div className='self-center col-span-3 md:col-span-1'>
+                <PrimaryHeader />
             </div>
         </div >
     )
