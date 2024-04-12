7636ba0b895b48a7e88240d8cbd49c4b3c873123
SobczakL <lucassobczak3@gmail.com>
2024-04-12 11:46:58 -0400

    added metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta property="og:title" content="Lucas Sobczak Portfolio" />
+                <meta property="og:description" content="Welcome to my portfolio! I'm Lucas Sobczak, a versatile developer with expertise in full-stack development. Dive into my collection of projects to explore the breadth of my skills and experience. From dynamic full-stack applications to intricate low-level programming projects, each entry demonstrates my ability to tackle diverse challenges and deliver high-quality solutions. Whether you're seeking a developer for your next web application, a collaborator on a cutting-edge software project, or simply interested in exploring innovative technology, I invite you to explore my portfolio and discover the depth of my capabilities." />
+                <meta property="og:image" content="images/headshot.png" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
